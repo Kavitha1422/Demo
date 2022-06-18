82401a48e4d88490cc49378fdd00c951f16eb34d
Kavitha1422 <kavtweety5@gmail.com>
2022-06-18 16:40:35 +0530

    new comments

---
+++ org/bank/AxisBank.java
@@ -0,0 +1,16 @@
+package org.bank;
+
+public class AxisBank extends BankInfo {
+	
+	public void deposit() {
+System.out.println("deposit is 30%");
+	}	
+	
+public static void main(String[] args) {
+	AxisBank ax = new AxisBank();
+	ax.deposit();
+	ax.fixed();
+	ax.saving();
+}}
+
+
