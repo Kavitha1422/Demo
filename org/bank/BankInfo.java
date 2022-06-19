b7a7164fe19b2744f4e4a6c1fdd5b4010576b310
Kavitha1422 <kavtweety5@gmail.com>
2022-06-19 15:12:43 +0530

    new branchcode added

--- org/bank/BankInfo.java
+++ org/bank/BankInfo.java
@@ -5,9 +5,7 @@ public class BankInfo {
 	public void saving() {
 System.out.println("saving is 50%");
 	}
-	public void fixed() {
-System.out.println("fixed is 30%");
-	}
+	
 	public void deposit() {
 System.out.println("deposit is 20%");
 	}
